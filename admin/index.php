9af241c216d86e8b7cee9c584b8289d07c9da0e0
Sumaiya Haque <85180703+Sumaiya-haque@users.noreply.github.com>
2022-05-25 00:06:26 +0600

    admin added

---
+++ admin/index.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <titl>food-shop website -Home Page</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2020 All rights reserved . Developed By - <a href="#">SNF</a></p>
+            </div>
+        </div>
+    
+
+    </body>
+</html>
